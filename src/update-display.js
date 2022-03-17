4ab7e9bc183a4f23eed38d5407ed866b3cdefdbe
bea <chamoischamois@protonmail.com>
2022-03-17 16:04:42 +0800

    Todo item details are displayed when clicked

--- src/update-display.js
+++ src/update-display.js
@@ -23,9 +23,17 @@ const displayNewItem = () => {
   newItemDiv.appendChild(dueDateDiv);
   document.getElementById('todo-items-container').appendChild(newItemDiv);
   newItemDiv.appendChild(DeleteTodoButton(fileArray[fileArray.length-1].title, fileArray[fileArray.length-1].fileName));
+  const itemTitle = fileArray[fileArray.length-1].title;
+  newItemDiv.addEventListener('click', () => {
+      displayItemDetails(itemTitle);
+  });
 }
 
 const displayFileItems = (currentFileName) => {
+    const todoItemContainer = document.getElementById('todo-item');
+    if (todoItemContainer.children.length) {
+        todoItemContainer.removeChild(todoItemContainer.firstChild);
+    };
     const todoItemsContainer = document.getElementById('todo-items-container');
     while (todoItemsContainer.firstChild) {
         todoItemsContainer.removeChild(todoItemsContainer.firstChild);
@@ -39,9 +47,38 @@ const displayFileItems = (currentFileName) => {
             itemDiv.appendChild(dueDateDiv);
             itemDiv.appendChild(DeleteTodoButton(fileArray[i].title, fileArray[i].fileName));
             document.getElementById('todo-items-container').appendChild(itemDiv);
+            itemDiv.addEventListener('click', () => {
+                const itemTitle = fileArray[i].title;
+                displayItemDetails(itemTitle);
+            });
             } 
         }
 }
 
+const displayItemDetails = (itemTitle) => {
+    const todoItemContainer = document.getElementById('todo-item');
+    if (todoItemContainer.children.length) {
+        todoItemContainer.removeChild(todoItemContainer.firstChild);
+    };
+    for (let i = 0; i < fileArray.length; i++) {
+        if (fileArray[i].title === itemTitle) {
+            const itemDetailsDiv = document.createElement('div');
+            const displayTitle = document.createElement('h3');
+            displayTitle.textContent = fileArray[i].title;
+            const displayDesc = document.createElement('p');
+            displayDesc.textContent = fileArray[i].desc;
+            const displayDue = document.createElement('p');
+            displayDue.textContent = `Due: ${fileArray[i].due}`;
+            const displayPriority = document.createElement('p');
+            displayPriority.textContent = `Priority: ${fileArray[i].priority}`;
+            itemDetailsDiv.appendChild(displayTitle);
+            itemDetailsDiv.appendChild(displayDesc);
+            itemDetailsDiv.appendChild(displayDue);
+            itemDetailsDiv.appendChild(displayPriority);
+            todoItemContainer.appendChild(itemDetailsDiv);
+        }
+    }
+
+}
 
 export { NewFile, displayNewItem, displayFileItems };

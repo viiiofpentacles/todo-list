5450f2611a650dbc94476fcc5bec1232f7b444a2
bea <chamoischamois@protonmail.com>
2022-03-18 20:59:09 +0800

    Added localStorage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import './style.css';
 import { NewFileButtonCreator, NewToDoItemButtonCreator } from './DOMbuttons';
-import { NewFile } from './update-display.js'
+import { displayFileItems, NewFile } from './update-display.js'
 import { NewItemForm } from './form.js';
+import { fileArray } from './todo-items.js';
+
+if (localStorage.length != 0) {
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        const todoItem = JSON.parse(localStorage.getItem(key));
+        fileArray.push(todoItem);
+    }
+}
 
 NewFileButtonCreator();
-NewFile('Notes');
+
+const displayStorageFiles = (() => {
+    const listOfFiles = [];
+    for (let i = 0; i < fileArray.length; i++) {
+        if (!listOfFiles.includes(fileArray[i].fileName)) {
+        listOfFiles.push(fileArray[i].fileName);
+        };
+    };
+    for (let i = 0; i < listOfFiles.length; i++) { 
+        NewFile(listOfFiles[i]);
+    }
+})();
+
+displayFileItems('Notes');
 const addButtonContainer = document.getElementById('add-button-container');
 addButtonContainer.appendChild(NewToDoItemButtonCreator());
 NewItemForm();
--- src/todo-items.js
+++ src/todo-items.js
@@ -8,13 +8,14 @@ const TodoItemCreator = function () {
       priority: (document.getElementById('priority').checked)? 'high': 'low',
       fileName: document.getElementById('fileName').value
     }
-
+    localStorage.setItem(todoItem.title, JSON.stringify(todoItem));
     fileArray.push(todoItem);
 }
 
 const deleteTodo = function (todoTitle) {
   for (let i = 0; i < fileArray.length; i++) {
     if (fileArray[i].title === todoTitle) {
+      localStorage.removeItem(todoTitle);
       fileArray.splice(i, 1);
     }
   }
